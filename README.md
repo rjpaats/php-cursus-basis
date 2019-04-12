f3ae7d1fabb31272e6f41d304f4615b13f32c75a
Paats, Rob <rob.paats@kpn.com>
2019-04-12 15:15:22 +0200

    Variabelen data toegevoegd. README van een update voorzien.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # php-cursus-basis
+
+Door bovenaan uw pagina’s error_reporting(E_ALL); op te nemen zal PHP een waarschuwing geven wanneer een variabele wordt gebruikt voordat u er een waarde aan heeft toegewezen. Dit is in de ontwikkelfase heel handig! U kunt het ook aangeven in php.ini.
+
+
---
+++ Variabelen/Variabelen.php
@@ -0,0 +1,26 @@
+<?php
+
+// In PHP kan je een variabel on de fly declareren en gebruiken. (Variabel begint ALTIJD met een $)
+// Het is niet noodzakelijk om hem een type mee te geven dit handeld php zelf af.
+
+// Variabel declareren zondere waarde mee te geven
+
+//Meerderre op één regel
+$geld; $aantal;
+
+//Meerdere op gescheiden regels
+$geld;
+$aantal;
+
+//Initiele waarde mee geven met de gedeclareerde variabel.
+$geld=100;
+$aantal=10;
+
+//Een helemaal lege variabel declareren waarbij hij initieel een lege waarde mee krijgt.
+$geld=NULL;
+$aantal=NULL;
+
+//Nullwaarde initieel in één go aan meerdere variabelen toe kennen.
+$geld = $aantal = NULL;
+
+
