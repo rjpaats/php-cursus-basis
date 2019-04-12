394ed8619f990e80eb325797c580a38d5a0e2613
Paats, Rob <rob.paats@kpn.com>
2019-04-12 15:02:42 +0200

    PHP Info gedefineerd En wat sommen met variabelen uitgetest

--- Reken Sommen/Reken Sommen.php
+++ Reken Sommen/Reken Sommen.php
@@ -1,20 +1,20 @@
 <?php
-
+?><BR></BR> <?php
 echo"Alles in één regel code!!!\n";
 $x=1;$y=21;$sum=$x+$y;echo $sum,"\n\n";
-
+?><BR></BR> <?php
 echo"En nu alle code op eigen regels\n";
 $x=1;
 $y=21;
 $sum=$x+$y;
 echo $sum,"\n\n";
-
+?><BR></BR> <?php
 echo"En nu met extra wit ruimte voor de leesbaarheid\n";
 $x      =   1;
 $y      =   21;
 $sum    =   $x + $y;
 echo    $sum, "\n\n";
-
+?><BR></BR> <?php
 echo"En nu met line breaks de leesbaarheid, php negeerd dit omdat de ; de line break is\n";
 $x      =   1;
 $y      =   21;
